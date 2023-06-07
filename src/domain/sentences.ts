c21ac91b78046be62f7e6305451d761d37e32f73
Yann Froment <yannfroment@MacBook-Pro-de-Yann.local>
2023-06-07 16:05:09 +0200

    refacto cutTextIntoSentences

--- src/domain/sentences.ts
+++ src/domain/sentences.ts
@@ -10,13 +10,6 @@ export const addMissingPunctuation = (rawText: string): string => {
   return text;
 };
 
-export const cutTextIntoSentences = (rawText: string): string[] => {
-  let text = rawText;
-
-  const lastCharacter = rawText.charAt(rawText.length - 1);
-  if (!PUNCTUATIONS.includes(lastCharacter)) {
-    text = rawText + '.';
-  }
-
-  return text.match(/[A-Z][^.!?]*[.!?]/g) ?? [];
+export const cutTextIntoSentences = (text: string): string[] => {
+  return addMissingPunctuation(text).match(/[A-Z][^.!?]*[.!?]/g) ?? [];
 };
