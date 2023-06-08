398e09e15500980e09e26ebc3e311aa3cd6ce8de
Yann Froment <yannfroment@MacBook-Pro-de-Yann.local>
2023-06-08 11:21:51 +0200

    GreenText

--- src/components/Sentence.test.tsx
+++ src/components/Sentence.test.tsx
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import Sentence from './Sentence';
+import Sentence, { GreenText } from './Sentence';
 
 describe('Sentence', () => {
   it('should display text passed as a sentence prop', async () => {
     render(<Sentence sentence={'Bonjour.'} />);
     expect(screen.queryByText('Bonjour.')).toBeInTheDocument();
+  });
+});
+
+describe('GreenText', () => {
+  it('should toto', async () => {
+    render(<GreenText sentence={'Bonjour.'} />);
+    expect(screen.queryByTestId('success')).toBeInTheDocument();
   });
 });
--- src/components/Sentence.tsx
+++ src/components/Sentence.tsx
@@ -6,4 +6,12 @@ const Sentence = ({ sentence }: SentenceProps) => {
   return <div>{sentence}</div>;
 };
 
+type TextProps = {
+  sentence: string;
+};
+
+export const GreenText = ({ sentence }: TextProps) => {
+  return <span data-testid="success">{sentence}</span>;
+};
+
 export default Sentence;
