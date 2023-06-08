9b6df880f98edf02fe0e6f4e1ecf6c29fb82303d
Yann Froment <yannfroment@MacBook-Pro-de-Yann.local>
2023-06-08 11:07:38 +0200

    sentence component test title

--- src/components/Sentence.test.tsx
+++ src/components/Sentence.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Sentence from './Sentence';
 
 describe('Sentence', () => {
-  it('foo', async () => {
+  it('should display text passed as a sentence prop', async () => {
     render(<Sentence sentence={'Bonjour.'} />);
     expect(screen.queryByText('Bonjour.')).toBeInTheDocument();
   });
