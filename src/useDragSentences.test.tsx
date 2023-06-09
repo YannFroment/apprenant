4b1ab6a3772834e37601fde84fd4e25202d150dd
Verdier <ldverdier@gmail.com>
2023-06-09 11:55:11 +0200

    implement pickFromRight

--- src/useDragSentences.test.tsx
+++ src/useDragSentences.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, waitFor } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 import { useDragSentences } from './useDragSentences';
 
 const initialSentences = ['sentence 1', 'sentence 2'];
@@ -15,6 +15,16 @@ describe('useDragSentences', () => {
       ),
     );
   });
+
+  it('should pick an available sentence', async () => {
+    const { result } = renderHook(useDragSentences, {
+      initialProps: initialSentences,
+    });
+
+    act(() => result.current.pickFromRight(0));
+
+    await waitFor(() => expect(result.current.pickedFromRightIndex).toBe(0));
+  });
 });
 
 /**
@@ -30,4 +40,13 @@ describe('useDragSentences', () => {
  * when a sentence is picked on the right side, the remaining elements go up
  * when a sentence is put back on the right side, it is appended at the end of the list
  * sentences cannot be rearranged directly on the left side: they must be put back from left to right first
+ *
+ * exposed methods:
+ * pickFromRight
+ *      should tell which of the available sentences is picked
+ *      only removed from available sentences when is put to left
+ * putToLeft
+ *      should remove the sentence from the list of available sentences
+ *      should specify at which position the sentence is put
+ *      should not accept to put a sentence in a position already occupied
  */
--- src/useDragSentences.tsx
+++ src/useDragSentences.tsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
+
 export const useDragSentences = (sentences: string[]) => {
+  const [pickedFromRightIndex, setPickedFromRightIndex] = useState<
+    number | undefined
+  >(undefined);
+  const pickFromRight = (index: number) => {
+    setPickedFromRightIndex(index);
+  };
+
   return {
     sentences,
+    pickFromRight,
+    pickedFromRightIndex,
   };
 };
