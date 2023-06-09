12cafdd4e9fdb3dca1e7ac23bdd1f3fd2d746d94
Verdier <ldverdier@gmail.com>
2023-06-09 11:16:33 +0200

    wire up test

--- src/useDragSentences.test.tsx
+++ src/useDragSentences.test.tsx
@@ -1,3 +1,16 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { useDragSentences } from './useDragSentences';
+
 describe('useDragSentences', () => {
-  it('should return available sentences', () => {});
+  it('should return available sentences', async () => {
+    const { result } = renderHook(useDragSentences, {
+      initialProps: ['sentence 1', 'sentence 2'],
+    });
+
+    await waitFor(() =>
+      expect(result.current.sentences).toEqual(
+        expect.arrayContaining(['sentence 1', 'sentence 2']),
+      ),
+    );
+  });
 });
