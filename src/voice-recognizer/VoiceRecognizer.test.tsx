acb375f0a5f037f2d26e456c295518a578731f89
Yann Froment <yannfroment@MacBook-Pro-de-Yann.local>
2023-06-14 10:17:53 +0200

    test click behavior

--- src/voice-recognizer/VoiceRecognizer.test.tsx
+++ src/voice-recognizer/VoiceRecognizer.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { VoiceRecognizer } from './VoiceRecognizer';
 
 describe('VoiceRecognizer', () => {
-  it('should do stuff', async () => {
+  it('should render the component', async () => {
     render(<VoiceRecognizer />);
 
-    expect(screen.queryByText('toto')).toBeInTheDocument();
+    expect(screen.queryByText('click me')).toBeInTheDocument();
+  });
+
+  it('should make a new dom element appear when button is clicked', async () => {
+    render(<VoiceRecognizer />);
+
+    await userEvent.click(screen.queryByText('click me')!);
+
+    expect(screen.queryByText('i was clicked')).toBeInTheDocument();
   });
 });
--- src/voice-recognizer/VoiceRecognizer.tsx
+++ src/voice-recognizer/VoiceRecognizer.tsx
@@ -1,3 +1,16 @@
+import { useState } from 'react';
+
 export const VoiceRecognizer = () => {
-  return <div>toto</div>;
+  const [buttonText, setButtonText] = useState('click me');
+
+  const handleClick = () => {
+    setButtonText('i was clicked');
+  };
+  return (
+    <div>
+      <button onClick={handleClick} type="button">
+        {buttonText}
+      </button>
+    </div>
+  );
 };
