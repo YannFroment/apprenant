8d9419520e4899fe9472247ea24318d1a122ccf6
Verdier <ldverdier@gmail.com>
2023-06-15 15:35:33 +0200

    fix defaultContainer

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,8 +1,10 @@
+import { Recorder } from '../src/voice-recognition/domain/Recorder';
 import { VoiceRecognitionServiceContainer } from '../src/voice-recognition/service-container/ServiceContainerContext';
 
 export const defaultContainer: VoiceRecognitionServiceContainer = {
   voiceRecognition: { recognize: () => true },
   speechSynth: { speak: () => {} },
+  recorder: () => ({} as ReturnType<Recorder>),
 };
 
 export const createContainer = (
