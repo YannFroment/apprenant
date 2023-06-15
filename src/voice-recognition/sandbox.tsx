9b2462d76f49bccc33b684ab9e9742a96aaf1a72
Verdier <ldverdier@gmail.com>
2023-06-15 12:12:41 +0200

    refactor sandbox component

--- src/voice-recognition/sandbox.tsx
+++ src/voice-recognition/sandbox.tsx
@@ -1,52 +1,69 @@
 import { useEffect, useState } from 'react';
 
+type Recognizer = {
+  start: () => void;
+  stop: () => void;
+};
+
+const recognizerFactory = ({
+  setTranscript,
+  setIsListening,
+}: {
+  setTranscript: (transcript: string) => void;
+  setIsListening: (isListening: boolean) => void;
+}): Recognizer => {
+  let recognition: SpeechRecognition | null = null;
+
+  const startRecognition = () => {
+    recognition = new (window.SpeechRecognition ||
+      window.webkitSpeechRecognition)();
+    recognition.continuous = false;
+    recognition.interimResults = false;
+    recognition.onresult = handleRecognitionResult;
+    recognition.onend = () => setIsListening(false);
+    recognition.start();
+    setIsListening(true);
+  };
+
+  const handleRecognitionResult = (event: SpeechRecognitionEvent) => {
+    const transcript = event.results[0][0].transcript;
+
+    setTranscript(transcript);
+  };
+
+  const stopRecognition = () => {
+    if (recognition) {
+      recognition.stop();
+      recognition.onresult = null;
+      setIsListening(false);
+    }
+  };
+
+  return {
+    start: startRecognition,
+    stop: stopRecognition,
+  };
+};
+
 export function SpeechRecognitionComponent() {
   const [transcript, setTranscript] = useState('');
   const [isListening, setIsListening] = useState(false);
 
   useEffect(() => {
-    let recognition: SpeechRecognition | null = null;
-
-    const startRecognition = () => {
-      recognition = new (window.SpeechRecognition ||
-        window.webkitSpeechRecognition)();
-      recognition.continuous = true;
-      recognition.interimResults = true;
-      recognition.onresult = handleRecognitionResult;
-      recognition.onend = () => setIsListening(false);
-      recognition.start();
-      setIsListening(true);
-    };
-
-    const handleRecognitionResult = (event: SpeechRecognitionEvent) => {
-      let finalTranscript = '';
-
-      for (let i = event.resultIndex; i < event.results.length; i++) {
-        const transcript = event.results[i][0].transcript;
-        if (event.results[i].isFinal) {
-          finalTranscript += transcript + ' ';
-        }
-      }
-
-      setTranscript(finalTranscript);
-    };
-
-    const stopRecognition = () => {
-      if (recognition) {
-        recognition.stop();
-        recognition.onresult = null;
-        setIsListening(false);
-      }
-    };
-
+    const { start, stop } = recognizerFactory({
+      setTranscript,
+      setIsListening,
+    });
     if (isListening) {
-      startRecognition();
+      start();
     }
 
     return () => {
-      stopRecognition();
+      stop();
     };
   }, [isListening]);
+
+  const word = 'chien';
 
   return (
     <div>
@@ -54,6 +71,7 @@ export function SpeechRecognitionComponent() {
         {isListening ? 'Stop Listening' : 'Start Listening'}
       </button>
       <p>Transcript: {transcript}</p>
+      <div>{word === transcript ? 'réussi !' : 'raté'}</div>
     </div>
   );
 }
