f2b3b89dfb6bdfee15e1c9252337c4d07fd30dda
Verdier <ldverdier@gmail.com>
2023-06-15 15:57:34 +0200

    refactor: rename variable

--- src/voice-recognition/SpeechRecorder.tsx
+++ src/voice-recognition/SpeechRecorder.tsx
@@ -11,26 +11,26 @@ export const SpeechRecorder = ({
   defaultIsRecording,
 }: SpeechRecorderProps) => {
   const [transcript, setTranscript] = useState('');
-  const [isListening, setIsListening] = useState<boolean>(
+  const [isRecording, setIsRecording] = useState<boolean>(
     defaultIsRecording ?? false,
   );
   const { recorder } = useContext(VoiceRecognitionContext);
 
   useEffect(() => {
     const { start, stop } = recorder(setTranscript);
-    if (isListening) {
+    if (isRecording) {
       start();
     }
 
     return () => {
       stop();
     };
-  }, [isListening, recorder]);
+  }, [isRecording, recorder]);
 
   return (
     <div data-testid={text}>
-      <button onClick={() => setIsListening(!isListening)}>
-        {isListening ? "Arrêter l'enregistrement" : 'Enregistrer'}
+      <button onClick={() => setIsRecording(!isRecording)}>
+        {isRecording ? "Arrêter l'enregistrement" : 'Enregistrer'}
       </button>
       <p>Transcript: {transcript}</p>
       <div>{text === transcript.toLowerCase() ? 'réussi !' : 'raté'}</div>
