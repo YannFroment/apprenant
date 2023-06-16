3479f3aff3449ad127c9c97db3bebf17872e4b13
Yann Froment <yannfroment@MacBook-Pro-de-Yann.local>
2023-06-16 09:30:05 +0200

    remove useless code

--- src/voice-recognition/VoiceRecognition.test.tsx
+++ src/voice-recognition/VoiceRecognition.test.tsx
@@ -2,7 +2,7 @@ import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { VoiceRecognition } from './VoiceRecognition';
 import { VoiceRecognitionContext } from './service-container/ServiceContainerContext';
-import { createContainer, defaultContainer } from '../../tests/utils';
+import { defaultContainer } from '../../tests/utils';
 
 describe('VoiceRecognition', () => {
   it('should display a component for each word of the list of words', async () => {
@@ -12,36 +12,6 @@ describe('VoiceRecognition', () => {
     expect(screen.queryByTestId('chat')).toBeInTheDocument();
     expect(screen.queryByTestId('chien')).toBeInTheDocument();
     expect(screen.queryByTestId('oiseau')).toBeInTheDocument();
-  });
-
-  it('should detect if recognition is a match', async () => {
-    const container = createContainer({
-      voiceRecognition: { recognize: () => true },
-    });
-    render(
-      <VoiceRecognitionContext.Provider value={container}>
-        <VoiceRecognition />
-      </VoiceRecognitionContext.Provider>,
-    );
-
-    await userEvent.click(screen.queryByText('click me')!);
-
-    expect(screen.queryByText('it is a match!')).toBeInTheDocument();
-  });
-
-  it('should detect if recognition is not a match', async () => {
-    const container = createContainer({
-      voiceRecognition: { recognize: () => false },
-    });
-    render(
-      <VoiceRecognitionContext.Provider value={container}>
-        <VoiceRecognition />
-      </VoiceRecognitionContext.Provider>,
-    );
-
-    await userEvent.click(screen.queryByText('click me')!);
-
-    expect(screen.queryByText('not a match!')).toBeInTheDocument();
   });
 
   it('should not change record button text for a word when clicking record button for another word', async () => {
--- src/voice-recognition/VoiceRecognition.tsx
+++ src/voice-recognition/VoiceRecognition.tsx
@@ -1,5 +1,3 @@
-import { useContext, useState } from 'react';
-import { VoiceRecognitionContext } from './service-container/ServiceContainerContext';
 import { Word } from './Word';
 
 type VoiceRecognitionProps = {
@@ -10,22 +8,8 @@ type VoiceRecognitionProps = {
 export const VoiceRecognition = (
   { words }: VoiceRecognitionProps = { words: [] },
 ) => {
-  const [buttonText, setButtonText] = useState('click me');
-
-  const { voiceRecognition } = useContext(VoiceRecognitionContext);
-
-  const handleClick = () => {
-    if (voiceRecognition.recognize()) {
-      return setButtonText('it is a match!');
-    }
-
-    return setButtonText('not a match!');
-  };
   return (
     <div>
-      <button onClick={handleClick} type="button">
-        {buttonText}
-      </button>
       {words?.map((word) => (
         <Word key={word} word={word} />
       ))}
