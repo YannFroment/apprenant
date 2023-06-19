8bcd4436d5d20886d68cc68dd83979ac79174eaa
Verdier <ldverdier@gmail.com>
2023-06-19 15:16:42 +0200

    refactor files

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { initialData } from './initial-data.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App initialData={initialData} />
+    <App />
   </React.StrictMode>,
 );
