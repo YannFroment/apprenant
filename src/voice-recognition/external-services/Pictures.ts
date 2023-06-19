b39bebb35b04b77aa263ec7d3972623a6f63245d
Yann Froment <yannfroment@MacBook-Pro-de-Yann.local>
2023-06-19 10:32:28 +0200

    wip

--- src/voice-recognition/external-services/Pictures.ts
+++ src/voice-recognition/external-services/Pictures.ts
@@ -1,7 +1,17 @@
 import { Pictures } from '../domain/Pictures';
+import { createClient } from 'pexels';
+
+const client = createClient(
+  'MxfAMFGikKpGKlWfKig5KPiffb7pimPKlKsAN7QBTMRBVsdujYQiws9j',
+);
 
 export const pictures: Pictures = {
   get: async (searchKey) => {
-    return searchKey;
+    const picture = await client.photos.search({
+      query: searchKey,
+      per_page: 1,
+    });
+    console.log(picture.photos);
+    return picture.photos[0].url;
   },
 };
