60aa8984372e11743b6a17f5f8aebc8e5b8e2e96
Verdier <ldverdier@gmail.com>
2023-06-26 15:27:13 +0200

    style footer

--- frontend/src/pages/layouts/Footer.tsx
+++ frontend/src/pages/layouts/Footer.tsx
@@ -3,8 +3,31 @@ import styled from 'styled-components';
 const StyledFooter = styled.footer`
   font-size: ${({ theme }) => theme.font.size.medium};
   height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.small};
+`;
+
+const FooterTitle = styled.h2`
+  letter-spacing: 1px;
+  font-size: ${({ theme }) => theme.font.size.small};
+`;
+
+const FooterText = styled.p`
+  font-size: ${({ theme }) => theme.font.size.small};
 `;
 
 export const Footer = () => {
-  return <StyledFooter>Mon footer</StyledFooter>;
+  return (
+    <StyledFooter>
+      <FooterTitle>NOUS SOMMES TOUS APPRENANTS</FooterTitle>
+      <FooterText>
+        Ce projet est inspiré des recherches de Francine Dessis, conseillère
+        pédagogique ainsi que des précieux travaux de Bernadette Gueritte Hess
+        et d'Elisabeth Pelloquin, expertes en pédagogie.
+      </FooterText>
+    </StyledFooter>
+  );
 };
