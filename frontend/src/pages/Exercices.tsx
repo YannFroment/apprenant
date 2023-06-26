6fe537cf2fae3df00398186e6fa55304a2cd5820
Verdier <ldverdier@gmail.com>
2023-06-26 15:32:35 +0200

    style Exercices

--- frontend/src/pages/Exercices.tsx
+++ frontend/src/pages/Exercices.tsx
@@ -1,17 +1,27 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { Layout } from './layouts/Layout';
+
+const ExercicesContainer = styled.div`
+  padding-top: 72px;
+  padding-left: 72px;
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.small};
+`;
 
 export const Exercices = () => {
   return (
     <Layout>
-      <Link to="/text-reorder" relative="path">
-        text-reorder
-      </Link>
-      <br />
-      <Link to="/voice-recognition" relative="path">
-        voice-recognition
-      </Link>
+      <ExercicesContainer>
+        <Link to="/text-reorder" relative="path">
+          Remettre le texte dans l'ordre
+        </Link>
+        <Link to="/voice-recognition" relative="path">
+          Reconnaître les mots
+        </Link>
+      </ExercicesContainer>
     </Layout>
   );
 };
