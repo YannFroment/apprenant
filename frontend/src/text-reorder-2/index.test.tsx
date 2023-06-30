b3fce0a78a6da7734956e79b7cc6f57cf27780d0
Yann Froment <yannfroment@MacBook-Pro-de-Yann.local>
2023-06-30 10:15:46 +0200

    init text reorder with route and store

--- frontend/src/text-reorder-2/index.test.tsx
+++ frontend/src/text-reorder-2/index.test.tsx
@@ -1,16 +1,40 @@
 import { screen } from '@testing-library/react';
 import { Route } from 'react-router-dom';
 
-import { renderWithinRoutes } from '../../tests/utils';
+import { renderWithinProviders, renderWithinRoutes } from '../../tests/utils';
+import { createUseStore } from '../store';
 import { TextReorder2 } from '.';
 
-test('renders with correct params', () => {
-  const id = '12';
-  renderWithinRoutes(
-    <Route path="/text-reorder/:id" element={<TextReorder2 />} />,
-    {},
-    [`/text-reorder/${id}`],
-  );
+describe('', () => {
+  it('should render with correct params', () => {
+    const id = '12';
+    renderWithinRoutes(
+      <Route path="/text-reorder/:id" element={<TextReorder2 />} />,
+      {},
+      [`/text-reorder/${id}`],
+    );
 
-  expect(screen.getByText('12')).toBeInTheDocument();
+    expect(screen.getByText('12')).toBeInTheDocument();
+  });
+  it('should retrieve the text reorder from the store', async () => {
+    const id = 12;
+
+    const customStore = createUseStore({
+      textReorders: [
+        {
+          id,
+          orderedSentences: ['a', 'b'],
+          randomizedSentences: ['a', 'b'],
+          title: 'toto',
+        },
+      ],
+    });
+    renderWithinRoutes(
+      <Route path="/text-reorder/:id" element={<TextReorder2 />} />,
+      { useStore: customStore },
+      [`/text-reorder/${id}`],
+    );
+
+    expect(screen.getByText('toto')).toBeInTheDocument();
+  });
 });
--- frontend/src/text-reorder-2/index.tsx
+++ frontend/src/text-reorder-2/index.tsx
@@ -1,9 +1,17 @@
 import { useParams } from 'react-router-dom';
 
 import { Layout } from '../pages/layouts/Layout';
+import { useTrainingsStore } from '../store';
 
 export const TextReorder2 = () => {
   const { id } = useParams();
+  const { textReorders } = useTrainingsStore();
+  const title = textReorders.find((el) => el.id.toString() === id)?.title;
 
-  return <Layout>{id}</Layout>;
+  return (
+    <Layout>
+      <p>{id}</p>
+      <div>{title}</div>
+    </Layout>
+  );
 };
