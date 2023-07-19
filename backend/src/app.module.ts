f053587e87a5f2a2d6652397ab49082f273a0df0
Verdier <ldverdier@gmail.com>
2023-07-19 10:41:36 +0200

    implement typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,13 +7,31 @@ import { ConcreteTextReorders } from './trainings/external-sources/TextReorders'
 import { WordRecognitionService } from './trainings/models/WordRecognition.service';
 import { WordRecognitions } from './trainings/models/WordRecognitions';
 import { ConcreteWordRecognitions } from './trainings/external-sources/WordRecognitions';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { UsersService } from './db-sandbox/user.service';
+import { UserSchema } from './db-sandbox/UserSchema';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'database',
+      port: 3306,
+      username: 'apprenant',
+      password: 'apprenant',
+      database: 'apprenant',
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true, // TODO TO BE REMOVED FOR PRODUCTION
+    }),
+    TypeOrmModule.forFeature([UserSchema]),
+  ],
   controllers: [AppController],
   providers: [
     HealthCheck,
     TextReorderService,
+    UsersService,
     {
       provide: TextReorders,
       useClass: ConcreteTextReorders,
@@ -25,4 +43,6 @@ import { ConcreteWordRecognitions } from './trainings/external-sources/WordRecog
     },
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {} // todo make it readonly?
+}
