687c40ad6f50070b081d06f7dc613089b43ee421
Verdier <ldverdier@gmail.com>
2023-07-20 16:06:09 +0200

    refactor

--- backend/src/persistence/datasource.ts
+++ backend/src/persistence/datasource.ts
@@ -8,10 +8,10 @@ const options: DataSourceOptions & SeederOptions = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  entities: [__dirname + '/../**/*.schema{.ts,.js}'],
+  entities: [__dirname + '/schemas/*.schema{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
-  seeds: [__dirname + '/seeds/*{.ts,.js}'],
-  factories: [__dirname + '/factories/*{.ts,.js}'],
+  seeds: [__dirname + '/seeds/*.seeder{.ts,.js}'],
+  factories: [__dirname + '/factories/*.factory{.ts,.js}'],
 };
 
 export const appDataSource = new DataSource(options);
