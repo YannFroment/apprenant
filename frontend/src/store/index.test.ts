e1c66b8265876e75d4486512f41e7d9188198f1f
Verdier <ldverdier@gmail.com>
2023-07-26 16:18:03 +0200

    add test on the useTrainingsStore hook

---
+++ frontend/src/store/index.test.ts
@@ -0,0 +1,26 @@
+import { act, renderHook } from '@testing-library/react';
+
+import { Trainings } from '../domain/Backend';
+import { useTrainingsStore } from '.';
+
+describe('useTrainingsStore', () => {
+  it('should update textReorders', () => {
+    const { result } = renderHook(useTrainingsStore);
+    const textReorders = [
+      {
+        id: 1,
+        title: 'title',
+        orderedSentences: ['a', 'b'],
+        randomizedSentences: ['b', 'a'],
+      },
+    ];
+    const trainings: Trainings = {
+      textReorders,
+      wordRecognitions: [],
+    };
+
+    act(() => result.current.setTrainings(trainings));
+
+    expect(result.current.textReorders).toEqual(textReorders);
+  });
+});
