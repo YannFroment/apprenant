88f24136141bc0fc32afcb594c07a0b9bad80dd1
Verdier <ldverdier@gmail.com>
2023-07-26 11:09:18 +0200

    add expect

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,6 +2,7 @@ import { screen, waitFor } from '@testing-library/react';
 
 import { inMemoryBackend, renderWithinProviders } from '../tests/utils';
 import App from './App';
+import { UseTrainingsStore } from './store';
 
 describe('Dashboard', () => {
   it('should display loader first, and display dashboard after data fetching', async () => {
@@ -18,15 +19,25 @@ describe('Dashboard', () => {
 
   it('should retrieve data from backend and save it in the store', async () => {
     const getTrainingsSpy = jest.spyOn(inMemoryBackend, 'getTrainings');
+    const trainingsStore = {
+      textReorders: [],
+      setTextReorders: () => {},
+    };
+    const setTrainingsSpy = jest.spyOn(trainingsStore, 'setTextReorders');
+
+    const useTrainingsStore: UseTrainingsStore = () => {
+      return trainingsStore;
+    };
 
     renderWithinProviders({
       children: <App />,
-      overrideServices: { backend: inMemoryBackend },
+      overrideServices: { backend: inMemoryBackend, useTrainingsStore },
       wrapInRouter: false,
     });
 
     await waitFor(() => {
       expect(getTrainingsSpy).toHaveBeenCalled();
+      expect(setTrainingsSpy).toHaveBeenCalled();
     });
   });
 });
