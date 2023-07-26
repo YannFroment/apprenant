7fbb6a2d69ab742ae249449a93b19309f56c6b81
Verdier <ldverdier@gmail.com>
2023-07-26 16:25:57 +0200

    refactor medias tests

--- frontend/src/trainings/word-recognition/Medias.test.tsx
+++ frontend/src/trainings/word-recognition/Medias.test.tsx
@@ -2,11 +2,17 @@ import { screen, waitFor, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { renderWithinProviders } from '../../../tests/utils';
+import { Word } from '../../domain/Backend';
 import { Medias } from './Medias';
+
+const createWords = (labels: string[]) =>
+  labels.map(
+    (label, index): Word => ({ id: index + 1, label, url: `${label}.jpg` }),
+  );
 
 describe('Medias', () => {
   it('should display a component for each word of the list of words', async () => {
-    const words = ['chat', 'chien', 'oiseau'];
+    const words: Word[] = createWords(['chat', 'chien', 'oiseau']);
     renderWithinProviders({ children: <Medias words={words} /> });
 
     await waitFor(() => {
@@ -18,7 +24,7 @@ describe('Medias', () => {
 
   it('should not change record button text for a word when clicking record button for another word', async () => {
     renderWithinProviders({
-      children: <Medias words={['chat', 'chien']} />,
+      children: <Medias words={createWords(['chat', 'chien'])} />,
     });
     await userEvent.click(
       within(screen.queryByTestId('chat')!).getByText('Enregistrer'),
--- frontend/src/trainings/word-recognition/Medias.tsx
+++ frontend/src/trainings/word-recognition/Medias.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import { Word } from '../../domain/Backend';
 import { Media } from './Media';
 
 const Container = styled.div`
@@ -9,14 +10,14 @@ const Container = styled.div`
 `;
 
 type MediasProps = {
-  words?: string[];
+  words?: Word[];
 };
 
 export const Medias = ({ words }: MediasProps = { words: [] }) => {
   return (
     <Container>
       {words?.map((word) => (
-        <Media key={word} word={word} />
+        <Media key={word.id} word={word} />
       ))}
     </Container>
   );
