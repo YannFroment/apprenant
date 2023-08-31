5dae58a28766a270c7338a1543b06242fc5bfa75
Verdier <ldverdier@gmail.com>
2023-08-31 14:20:55 +0200

    add strategy and guard

---
+++ backend/src/auth/jwtRefresh.strategy.ts
@@ -0,0 +1,26 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_REFRESH_SECRET,
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: any) {
+    const refreshToken = req
+      .get('Authorization')! // TODO remove the bang
+      .replace('Bearer', '')
+      .trim();
+    return { ...payload, refreshToken };
+  }
+}
